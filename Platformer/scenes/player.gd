ba6f07a9b03077ca214bd9271ab8a32551d0af99
codeSmith12 <89124403+codeSmith12@users.noreply.github.com>
2026-09-16 10:57:15 -0700

    Lesson 1 - Added floor and player that can move and jump

---
+++ Platformer/scenes/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var speed = 250.0
+var jump_velocity = -400.0
+var gravity = 980.0
+
+
+func _physics_process(delta):
+	# Fall faster over time.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Left gives -1, right gives 1, neither gives 0.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * speed
+
+	# Jump only while standing on the floor.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	move_and_slide()
